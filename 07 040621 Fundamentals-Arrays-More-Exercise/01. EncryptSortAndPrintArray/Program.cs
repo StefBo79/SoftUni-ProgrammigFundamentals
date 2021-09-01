8df7acbba7e950d80cf36e4bb545ea3066bf7076
StefBo <bonev@x-spot.org>
2021-09-01 22:20:18 +0300

    07 040621

---
+++ 07 040621 Fundamentals-Arrays-More-Exercise/01. EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _01._EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int[] sum = new int[numbers];
+            for (int i = 0; i < numbers; i++)
+            {
+                string names = Console.ReadLine();
+                sum[i] = sumVowelsConsonants(names);
+            }
+            Array.Sort(sum);
+
+            foreach (var item in sum)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int sumVowelsConsonants(string text)
+        {
+                  int sum = 0;
+                  foreach (char element in text)
+                  {
+                      if (element == 'a' || element == 'A' || element == 'e' || element == 'E' || element == 'i' || element == 'I' || element == 'u' || element == 'U' || element == 'o' || element == 'O')
+                      {
+                          sum += (int)element * text.Length;
+                      }
+                      else
+                      {
+                          sum += (int)element / text.Length;
+                      }
+                  }
+              
+                  return sum;
+              }
+        }
+    }
---
+++ 07 040621 Fundamentals-Arrays-More-Exercise/03. RecursiveFibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
