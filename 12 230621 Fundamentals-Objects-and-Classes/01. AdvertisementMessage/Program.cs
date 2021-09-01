670eb0df7b91bc25785b3c189fb11e6e268df3b2
StefBo <bonev@x-spot.org>
2021-09-01 22:43:53 +0300

    12 230621

---
+++ 12 230621 Fundamentals-Objects-and-Classes/01. AdvertisementMessage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = new List<int>();
+
+            List<int> secondList = new List<int>();
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                int[] commandArg = 
+            }
+                
+
+            Console.WriteLine(String.Join(" ", firstList, secondList));
+
+        }
+    }
+}
