03f98842145265d2b3831da7265bba1319a289a4
Stefan Bonev <bonev@x-spot.org>
2021-09-08 21:01:42 +0300

    23 140821

---
+++ 23 140821 FinalExam/Problem1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string command = Console.ReadLine();
+            while (command != "Finish")
+            {
+                string[] commandArgs = command.Split();
+                string action = commandArgs[0];
+
+                if (action == "Replace")
+                {
+                    string currentChar = commandArgs[1];
+                    string newChar = commandArgs[2];
+
+                    while (message.Contains(currentChar))
+                    {
+                        message = message.Replace(currentChar, newChar);
+                    }
+
+                    Console.WriteLine(message);
+                }
+
+                if (action == "Cut")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+
+                    if (startIndex >= 0 && startIndex < message.Length && endIndex >= 0 && endIndex < message.Length)
+                    {
+                        message = message.Remove(startIndex, endIndex - startIndex + 1);
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid indices!");
+                    }
+                }
+
+                if (action == "Make")
+                {
+                    string UpperLower = commandArgs[1];
+
+                    if (UpperLower == "Upper")
+                    {
+                        message = message.ToUpper();
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        message = message.ToLower();
+                        Console.WriteLine(message);
+                    }
+                }
+
+                if (action == "Check")
+                {
+                    string checkedMessage = commandArgs[1];
+
+                    if (message.Contains(checkedMessage))
+                    {
+                        Console.WriteLine($"Message contains {checkedMessage}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Message doesn't contain {checkedMessage}");
+                    }
+
+                }
+
+                if (action == "Sum")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+                    int length = (endIndex - startIndex) + 1;
+                    int sum = 0;
+
+                    if (startIndex > 0 && endIndex < message.Length)
+                    {
+                        string substring = message.Substring(startIndex, length);
+
+                        for (int i = 0; i < substring.Length; i++)
+                        {
+                            sum += substring[i];
+                        }
+
+                        Console.WriteLine(sum);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid indices!");
+                    }
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 23 140821 FinalExam/Problem3/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> userSent = new Dictionary<string, int>();
+            Dictionary<string, int> userReceived = new Dictionary<string, int>();
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            while (command != "Statistics")
+            {
+                string[] commandArgs = command.Split("=");
+                string action = commandArgs[0];
+
+                if (action == "Add")
+                {
+                    string username = commandArgs[1];
+                    int sent = int.Parse(commandArgs[2]);
+                    int received = int.Parse(commandArgs[3]);
+
+                    if (!userSent.ContainsKey(username))
+                    {
+                        userSent.Add(username, sent);
+                        userReceived.Add(username, received);
+                    }
+                    else
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }                    
+                }
+
+                if (action == "Message")
+                {
+                    string sender = commandArgs[1];
+                    string receiver = commandArgs[2];
+
+                    if (userSent.ContainsKey(sender) && userReceived.ContainsKey(receiver))
+                    {
+                        userSent[sender] += 1;
+                        userReceived[receiver] += 1;
+
+                        if (userSent[sender] >= capacity || userReceived[sender] >= capacity || userSent[sender] + userReceived[sender] >= capacity)
+                        {
+                            Console.WriteLine($"{sender} reached the capacity!");
+                            userSent.Remove(sender);
+                            userReceived.Remove(sender);
+                        }
+
+                        if (userReceived[receiver] >= capacity || userSent[receiver] >= capacity || userSent[receiver] + userReceived[receiver] >= capacity)
+                        {
+                            Console.WriteLine($"{receiver} reached the capacity!");
+                            userSent.Remove(receiver);
+                            userReceived.Remove(receiver);
+                        }                        
+                    }
+                }
+
+                if (action == "Empty")
+                {
+                    string username = commandArgs[1];
+                    if (userSent.ContainsKey(username))
+                    {
+                        userSent.Remove(username);
+                        userReceived.Remove(username);
+                    }
+                    else if (username == "All")
+                    {
+                        userSent = new Dictionary<string, int>();
+                        userReceived = new Dictionary<string, int>();
+                    }                                       
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join("", $"Users count: {userSent.Count}"));
+
+            userSent = userSent
+                .OrderByDescending(u => userReceived[u.Key])
+                .ThenBy(u => u.Key)
+                .ToDictionary(u => u.Key, u => u.Value);
+
+
+            foreach (var user in userSent)
+            {
+                Console.WriteLine($"{user.Key} - {user.Value + userReceived[user.Key]}");
+
+            }
+        }
+    }
+}
